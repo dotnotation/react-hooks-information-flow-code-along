32f4416f831f00899d3247e6bcf5f7951fe03f76
dotnotation <dotinmotionproductions@gmail.com>
2022-04-20 15:09:30 -0400

    completed code along

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,21 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      {/* passing the state changing function as a prop to both child components */}
+      {/* now child will have a prop called onChangeColor that is a function that is the same function object as the parent's handleChangeColor */}
+      {/* passing the childrenColor state so that siblings can have access to the same props */}
     </div>
   );
 }
